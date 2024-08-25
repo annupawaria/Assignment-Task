96802c6a9a25c264ba452de37e77cf691a033f44
annu <annupawadia@gmail.com>
2024-08-25 16:50:14 +0530

    applying in a different way

--- src/App.js
+++ src/App.js
@@ -6,8 +6,25 @@ import './App.css'
 const App = () => {
   return (
     <div>
-      <Services/>
-      {/* <Category/> */}
+     Nav
+     <div>
+     <a href='#Home'>Home</a>
+     <a href='#contact'>Contact</a>
+     <a href='#about'>About</a>
+     <a href='#hello'>hello</a>
+     </div>
+     <section id="Home">
+<h1>Home</h1>
+     </section>
+     <section id="contact">
+<h1>Home</h1>
+     </section>
+     <section id="about">
+<h1>about</h1>
+     </section>
+     <section id="hello">
+<h1>hello</h1>
+     </section>
       </div>
   )
 }
